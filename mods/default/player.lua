6a6b8398aa53fc72a23a954469de5ff50be94074
BlockMen <nmuelll@web.de>
2013-12-11 15:25:51 +0100

    New hand

--- mods/default/player.lua
+++ mods/default/player.lua
@@ -212,3 +212,40 @@ minetest.register_globalstep(function(dtime)
 		end
 	end
 end)
+
+--
+-- the hand
+--
+
+local tool_cap = {
+		full_punch_interval = 1.0,
+		max_drop_level = 0,
+		groupcaps = {
+			fleshy = {times={[2]=2.00, [3]=1.00}, uses=0, maxlevel=1},
+			crumbly = {times={[2]=3.00, [3]=0.70}, uses=0, maxlevel=1},
+			snappy = {times={[3]=0.40}, uses=0, maxlevel=1},
+			oddly_breakable_by_hand = {times={[1]=7.00,[2]=4.00,[3]=1.40}, uses=0, maxlevel=3},
+		}
+	}
+if minetest.setting_getbool("creative_mode") then
+tool_cap = {	full_punch_interval = 0.5,
+		max_drop_level = 3,
+		groupcaps = {
+			crumbly = {times={[1]=0.3, [2]=0.3, [3]=0.3}, uses=0, maxlevel=3},
+			cracky = {times={[1]=0.3, [2]=0.3, [3]=0.3}, uses=0, maxlevel=3},
+			snappy = {times={[1]=0.3, [2]=0.3, [3]=0.3}, uses=0, maxlevel=3},
+			choppy = {times={[1]=0.3, [2]=0.3, [3]=0.3}, uses=0, maxlevel=3},
+			oddly_breakable_by_hand = {times={[1]=0.3, [2]=0.3, [3]=0.3}, uses=0, maxlevel=3},
+		},
+		damage_groups = {fleshy = 10},
+	}
+end
+
+minetest.after(0, function()
+	minetest.register_item(":", {
+		type = "none",
+		wield_image = minetest.inventorycube("hand_top.png", "hand_left.png", "hand_right.png"),
+		wield_scale = {x=0.6,y=1.1,z=3.8},
+		tool_capabilities = tool_cap,
+	})
+end)
